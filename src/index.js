b70dc6e7a7d117e471150a22043cad4db6a24a9c
ranapat <ranapat@gmail.com>
2017-05-23 18:07:24 +0200

    include collection if anyone wants to extend it

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import Factory from './factory/Factory';
 import Map from './factory/Map';
 import Match from './factory/Match';
 
+import Collection from './chains/Collection';
 import Sequence from './chains/Sequence';
 import Parallel from './chains/Parallel';
 import Loop from './chains/Loop';
 
 export { Task, TimeoutTask, TriggerTask };
-export { Sequence, Parallel, Loop };
+export { Collection, Sequence, Parallel, Loop };
 export { Factory, Factory as tf };
 export { Map };
 export { Match };
