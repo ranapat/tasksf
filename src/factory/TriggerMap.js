0528e8fe7ce5f6a02fddd7167a82d8a825bd08fd
ranapat <ranapat@gmail.com>
2017-06-02 15:13:23 +0200

    trigger map fixes

--- src/factory/TriggerMap.js
+++ src/factory/TriggerMap.js
@@ -3,16 +3,16 @@ import Match from './Match';
 import TriggerTask from '../tasks/TriggerTask';
 
 /**
- * Map for TriggerMap
+ * Map for TriggerTask
  *
- * Maps the default ways to get a TriggerMap
+ * Maps the default ways to get a TriggerTask
  */
 class TriggerMap extends Map {
   /**
-   * Match argument with a specific TriggerMap
+   * Match argument with a specific TriggerTask
    *
    * @param {Function} complete function to be called on complete
-   * @param {boolean} marker marks a trigger task
+   * @param {boolean} marker false marks a trigger task
    * @return {Match} match
    */
   match(...args) {
@@ -21,11 +21,11 @@ class TriggerMap extends Map {
       && typeof args[0] === 'function'
       && args[1] === false
     ) {
-      return new Match(new TriggerTask(...args));
+      return new Match(new TriggerTask(args[0]));
     } else if (
       args.length === 0
     ) {
-      return new Match(new TriggerTask(...args), 0);
+      return new Match(new TriggerTask(), 0);
     } else {
       return undefined;
     }
