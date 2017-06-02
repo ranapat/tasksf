da5a16157bb7cc77b0b36c1fe6390f7be860dcd9
ranapat <ranapat@gmail.com>
2017-06-02 15:57:22 +0200

    async map

---
+++ src/factory/AsyncMap.js
@@ -0,0 +1,48 @@
+import Map from './Map';
+import Match from './Match';
+import AsyncTask from '../tasks/AsyncTask';
+
+/**
+ * Map for AsyncTask
+ *
+ * Maps the default ways to get a AsyncTask
+ */
+class AsyncMap extends Map {
+  /**
+   * Match argument with a specific TriggerMap
+   *
+   * @param {Function} run function to be called on run
+   * @param {Function} complete function to be called on complete
+   * @param {Function} recover function to be called on recover
+   * @param {number} marker 0 marks a trigger task
+   * @return {Match} match
+   */
+  match(...args) {
+    if (
+      args.length === 4
+      && typeof args[0] === 'function'
+      && typeof args[1] === 'function'
+      && typeof args[2] === 'function'
+      && args[3] === 0
+    ) {
+      return new Match(new AsyncTask(...args));
+    } else if (
+      args.length === 3
+      && typeof args[0] === 'function'
+      && typeof args[1] === 'function'
+      && args[2] === 0
+    ) {
+      return new Match(new AsyncTask(...args));
+    } else if (
+      args.length === 2
+      && typeof args[0] === 'function'
+      && args[1] === 0
+    ) {
+      return new Match(new AsyncTask(...args));
+    } else {
+      return undefined;
+    }
+  }
+}
+
+export default AsyncMap;
